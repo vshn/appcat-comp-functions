e635ee62fe24cb470076efa143c29aec787939b4
Fabian Fischer <glrf@users.noreply.github.com>
2023-05-03 10:33:23 +0200

    Add generated stackgres structs for SGDbOps and SGCluster

--- tools.go
+++ tools.go
@@ -6,5 +6,7 @@
 package tools
 
 import (
-// Add any build-time dependencies here with blank imports like `_ "package"`
+	// Add any build-time dependencies here with blank imports like `_ "package"`
+	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
